fb6bc7cee0cdbc9003a6e0ed661a06a6c24ae58c
Tyler Satkowiak <tylersatkowiak@quickenloans.com>
2020-07-14 14:48:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+namespace Lab_3._2_Shopping_List
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> items = new Dictionary<string, int>();
+
+
+
+        }
+    }
+}
